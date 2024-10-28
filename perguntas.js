9aa12df814ac09e3f130e38214bf235558db6b38
Douglas-Silva-Raixz <162138549+Douglas-Silva-Raixz@users.noreply.github.com>
2024-10-28 11:32:53 -0300

    x

---
+++ perguntas.js
@@ -0,0 +1,51 @@
+criarCartao(
+    'Matemática',
+    'Como se calcula a área de um cilindro?',
+    'Utilizando a fórmula 2π r h + 2π r².'
+)
+
+criarCartao(
+    'Matemática',
+    'O que é uma função quadrática?',
+    'Também chamada de função do 2° grau, é uma função que contém os valores a, b e c, diferente da função do 1° grau que possui somente a e b.'
+)
+
+criarCartao(
+    'Matemática',
+    'O que é uma matriz numérica?',
+    'Números reais organizados em linhas e colunas com o objetivo de facilitar cálculos'
+)
+criarCartao(
+    'Química',
+    'O que são os anéis aromáticos?',
+    'São estruturas formadas por seis átomos de carbono ligados entre si por ligações simples e duplas alternadas.'
+)
+
+criarCartao(
+    'Química',
+    'O que é um ácido carboxílico?',
+    'Ácido que contém a carboxila, junção entre a carbonila (C=O) e a hidroxila (O-H)'
+)
+
+criarCartao(
+    'Química',
+    'Como é feito o refinamento do petróleo??',
+    'Através da destilação fracionada, que separa a parte mais densa (piche e óleos) da menos densa (combustíveis e gas)'
+)
+criarCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criarCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criarCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
