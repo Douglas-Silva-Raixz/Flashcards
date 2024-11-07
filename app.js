90be6733437e762ac0d9273a2085ed8bd23f720b
Douglas-Silva-Raixz <162138549+Douglas-Silva-Raixz@users.noreply.github.com>
2024-11-07 07:53:39 -0300

    f

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function criarCartao(categoria, pergunta, resposta){
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+}
     cartao.innerHTML = `
     <div class="conteudo">
                     <h3>${categoria}</h3>
